60463434473e967e97c3614233d5eda13173b8d5
Jason Tucker <jtucker@gmail.com>
2023-09-06 09:53:52 -0400

    Update .chezmoiignore Add TODO.md Add .config/powershell/profile/102.winget-completion.ps1 Update install-windows.ps1

---
+++ TODO.md
@@ -0,0 +1,15 @@
+# Items I want to add to this repo
+
+### `run_once_before_install-windows.ps1` additions
+- Add a check to elevate into PWSH/Terminal over PowerShell
+- Add winget install process
+- Add `Set-ExcutionPolicy`
+
+### `winget-install.json` additions
+- Script to intermittently export software versions? 
+
+### Other
+- Add terminal settings
+- Add vscode settings
+- Add editorconfig settings
+- Add preferred fonts
--- run_once_before_install-windows.ps1
+++ run_once_before_install-windows.ps1
@@ -1,2 +1,13 @@
 
-Write-Host "Hello, World! From Script" -ForegroundColor Green
+Write-Host "🎉 First run install, need to setup some pre-reqs" -ForegroundColor Green
+
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+        Start-Process -Wait -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+        Exit
+    }
+}
+
+Write-Host "⭐ Admin level achieved.." -ForegroundColor DarkRed
