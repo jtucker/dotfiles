4337e70dc971cab4360a5e1a218afd9944460c5c
Jason Tucker <jtucker@gmail.com>
2024-02-14 10:56:09 -0500

    cleaned up some of the elevation logic and added font installing.

--- run_once_before_install-windows.ps1
+++ run_once_before_install-windows.ps1
@@ -1,21 +1,47 @@
 
 Write-Host "🎉 First run install, need to setup some pre-reqs" -ForegroundColor Green
 
-# Self-elevate the script if required
-if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-        $CommandLine = "-ExecutionPolicy Unrestricted -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-        Start-Process -Wait -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-        Exit
-    }
+if(!(Get-Command winget -ErrorAction SilentlyContinue)) {
+    Write-Error "winget not found. Exiting"
+    return
 }
 
-Write-Host "⭐ Admin level achieved.." -ForegroundColor DarkRed
-Set-ExecutionPolicy Unrestricted -ErrorAction Stop
+Write-Host "⌛ Updating winget source"
+winget source update
 
-if(Get-Command winget -ErrorAction SilentlyContinue) {
-    Write-Host "🖥️ Installing software from winget"
+## check for git
+if(!(Get-Command git -ErrorAction SilentlyContinue)) {
+    Write-Host "🖥️ Installing Git"
+    winget install --id Git.Git `
+        --custom '/components="gitlfs,scalar" /o:EditorOption=VisualStudioCode /o:DefaultBranchOption=main /o:PathOption=Cmd /o:SSHOption=ExternalOpenSSH /o:CurlOption=WinSSL /o:BashTerminalOption=ConHost /o:GitPullBehaviorOption=Rebase /o:UseCredentialManager=Enabled' `
+        --silent --accept-package-agreements --accept-source-agreements
+    Write-Host "🖥️ Installed Git"
+}
+
+## Check for sudo
+if(!(Get-Command sudo -ErrorAction SilentlyContinue)) {
+    Write-Host "🖥️ Installing sudo"
+    winget install --id gerardog.gsudo `
+        --silent --accept-package-agreements --accept-source-agreements
+    Write-Host "🖥️ Installed sudo"
+}
+
+# Update the path
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+gsudo {
+    Write-Host "⭐ Admin level achieved.." -ForegroundColor DarkRed
+    Set-ExecutionPolicy Unrestricted -ErrorAction Stop
+
+    Write-Host "🖥️ Installing software from winget" -ForegroundColor DarkRed
     winget import -i "$(chezmoi source-path)/winget-install.json" --accept-source-agreements --accept-package-agreements --no-upgrade --disable-interactivity
-} else {
-    Write-Host "No winget installed"
+
+    Write-Host "✪ Installing fonts" -ForegroundColor DarkRed
+    if(Test-Path ~/OneDrive) {
+        $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+        Get-ChildItem ~/OneDrive/Fonts/current-font/*.ttf | ForEach-Object { 
+            Write-Host "`tInstalling font: $($_.FullName)" -ForegroundColor DarkGray
+            $fonts.CopyHere($_.FullName) 
+        }
+    }
 }
