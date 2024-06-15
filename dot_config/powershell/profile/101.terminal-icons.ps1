d4d42416b64827c55739a279266ee3f8d2ef4333
Jason Tucker <jtucker@gmail.com>
2024-06-14 20:03:07 -0700

    removing terminal-icons because slows terminal

--- dot_config/powershell/profile/101.terminal-icons.ps1
+++
@@ -1,6 +0,0 @@
-
-if (!(Get-InstalledModule Terminal-Icons -ErrorAction SilentlyContinue)) {
-    Install-Module Terminal-Icons -Force -Scope CurrentUser -SkipPublisherCheck
-}
-
-Import-Module -Name Terminal-Icons
