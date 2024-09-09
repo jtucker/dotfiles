8a5f61a885e8864cbe1d68e6bd9062b8c5700193
Jason Tucker <jatucke@microsoft.com>
2024-09-09 12:55:57 -0400

    Update .config/powershell/profile/105.cli-tools.ps1

--- dot_config/powershell/profile/105.cli-tools.ps1
+++ dot_config/powershell/profile/105.cli-tools.ps1
@@ -6,4 +6,18 @@ else {
     Import-Module -Name PSFzf
 }
 
+# Set FZF for PsReadline Provider
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PSReadLineOption -PredictionSource History
+
+# Set eza aliases
+function Invoke-Eza {
+    eza --long --header --all --icons --git --group-directories-first --git-repos
+}
+
+
+Remove-Alias -Name ls
+Set-Alias -Name ls -Value Invoke-Eza
+Set-Alias -Name ll -Value Invoke-Eza
+Set-Alias -Name la -Value Invoke-Eza
 
